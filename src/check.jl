d2d5867f2077eeeb1f2295925c8289f1ad442355
Klaus Crusius <klaus.crusius@web.de>
2017-08-04 17:36:29 +0200

    added tests

---
+++ src/check.jl
@@ -0,0 +1,55 @@
+
+"""
+    is_ordered(T::Type)
+
+Return true iff leaftype `T` is ordered (either totally or partially).
+Method: Check if method `<(::T,::T) is available at runtime
+"""
+is_ordered(::Type{T}) where {T<:Any} = checkm(T, <)
+
+"""
+    is_partially_ordered(T::Type)
+
+Return true iff leaftype `T` is partially ordered.
+Method: Check if method `<(::T,::T) is available and `isless(::T,::T)` is not.
+"""
+is_partially_ordered(::Type{T}) where {T<:Any} = checkm(T, <) && ! checkm(T, isless)
+
+"""
+    is_totally_ordered(T::Type)
+
+Return true iff `T` is totally ordered.
+Method: Check if methods `<(::T,::T) ans `isless(::T,::T)` are available.
+"""
+is_totally_ordered(::Type{T}) where {T<:Any} = checkm(T, <) && checkm(T, isless)
+
+"""
+  is_iterable(::Type)
+
+Return true iff leaf type `T` has methods `start(T)`, `next(T, Any)`, and `done(T, Any)`.
+"""
+is_iterable(::Type{T}) where {T<:Any} = hasmethod(start, (T,)) && hasmethod(done, (T, Any)) && hasmethod(next, (T, Any))
+ 
+### Implementation details
+
+checkm(t::Type{T}, f::Function) where T = hasmethod(f, (T, T))
+
+function hasmethod(f::Function, t = Tuple)
+  if all(isleaftype, t)
+    m = methods(f, t)
+    !isempty(m)
+  else
+    false
+  end
+end
+
+"""
+  partition(f, a::AbstractArray) -> (filter(f, a), filter( !f, a))
+
+Partition the elements of array according to predicate function `f`.
+The function is only evaluated once per array element.
+"""
+function partition(f, As::AbstractArray)
+   sel = map(f, As)::AbstractArray{Bool}
+   As[sel], As[.!(sel)]
+ end
